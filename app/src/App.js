0ba0bc3a1dbf304ad285303a210c8439eb98cca5
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-08-27 10:29:53 -0300

    Aula 16 - Estrutura de pastas

--- app/src/App.js
+++ app/src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React JS</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
