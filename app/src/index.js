0ef090a281267d174742b2f18e4f08de3935b0a3
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-08-27 11:30:42 -0300

    Aula 19 - Configurações do VSCode

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Greetering () {
-  return <h2>My First Component</h2>   
+function Greetering() {
+  return <h2>My First Component</h2>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
 root.render(<Greetering />)
