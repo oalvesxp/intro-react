06d10de6559ab5bb4fbda3a1bcc5aa26c1c93041
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-08-27 11:41:41 -0300

    Aula 20 - Função createElement

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function Greetering() {
-  return <h2>My First Component</h2>
+  // return <h2>My First Component</h2>
+  // return React.createElement('h2', {}, 'My First Component')
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, 'My First Component')
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
