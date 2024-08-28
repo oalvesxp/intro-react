f58c69fdecaa77bc088c8c7d621b8dcfd2642209
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-08-28 14:28:05 -0300

    Aula 48 - Desafio: Número de livros

--- app/src/Book.js
+++ app/src/Book.js
@@ -1,10 +1,11 @@
-const Book = ({ img, title, author, getBook, id }) => {
+const Book = ({ img, title, author, getBook, id, number }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
       <button onClick={() => getBook(id)}>Display</button>
+      <span className="number">{`# ${number + 1}`}</span>
     </article>
   )
 }
--- app/src/index.js
+++ app/src/index.js
@@ -12,8 +12,8 @@ const BookList = () => {
 
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} getBook={getBook} number={index} />
       })}
     </section>
   )
