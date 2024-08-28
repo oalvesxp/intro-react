2a1738cf4bad5465f0935e169f5edf2f58453059
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-08-28 08:54:35 -0300

    Aula 33.34 - Mapeamento de objetos

--- app/src/index.js
+++ app/src/index.js
@@ -5,50 +5,31 @@ import book2 from './assets/book-2.jpg'
 import book3 from './assets/book-3.jpg'
 import './index.css'
 
-const first = {
-  title: 'A Lenda de Ruff Ghanor: O Garoto-Cabra',
-  author: 'Leonel Caldela',
-  img: book1,
-}
-
-const second = {
-  title: 'OZOB: Protocolo Molotov',
-  author: 'Deive Pazos',
-  img: book2,
-}
-
-const third = {
-  title: 'O cavaleiro dos Sete Reinos',
-  author: 'George R. R. Martin',
-  img: book3,
-}
+const books = [
+  {
+    title: 'A Lenda de Ruff Ghanor: O Garoto-Cabra',
+    author: 'Leonel Caldela',
+    img: book1,
+  },
+  {
+    title: 'OZOB: Protocolo Molotov',
+    author: 'Deive Pazos',
+    img: book2,
+  },
+  {
+    title: 'O cavaleiro dos Sete Reinos',
+    author: 'George R. R. Martin',
+    img: book3,
+  },
+]
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book img={first.img} title={first.title} author={first.author}>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          architecto magnam non. Autem, corporis. Quas earum consectetur
-          assumenda explicabo sapiente.
-        </p>
-        <button>Saiba Mais</button>
-      </Book>
-      <Book img={second.img} title={second.title} author={second.author}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-          commodi voluptatum culpa praesentium et laborum.
-        </p>
-        <button>Saiba Mais</button>
-      </Book>
-      <Book img={third.img} title={third.title} author={third.author}>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
-          blanditiis asperiores voluptates suscipit beatae nemo magnam
-          provident.
-        </p>
-        <button>Saiba Mais</button>
-      </Book>
+      {books.map((book) => {
+        const { img, title, author } = book
+        return <Book img={img} title={title} author={author} />
+      })}
     </section>
   )
 }
